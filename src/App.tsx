7ed0299eb1bc7ce09eb4d650e0d9853f89e60301
Piotr Pustul <piotrpustul1@gmail.com>
2023-08-20 14:33:12 +0100

    added delete todo functionality, added css styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,22 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
     setTodos((prevTodos) => [
       ...prevTodos,
       { id: Math.random().toString(), text: text },
     ]);
   };
 
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div className="App">
       <NewToDo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
